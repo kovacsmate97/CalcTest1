70805216df964699d9cfa1570a5fac65d61cca0e
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-09 12:17:51 +0100

    Esemény bekötése gombra

--- CalcTest1/Form1.cs
+++ CalcTest1/Form1.cs
@@ -16,5 +16,10 @@ namespace CalcTest1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ""+Calculator.Osszead(5, 6);
+        }
     }
 }
